ae63784239c62a1a3cf6c9af404e1432318eea5d
Mitfart <mr.mitfart@mail.ru>
2026-08-24 22:12:30 +0300

    Publish portfolio site

---
+++ _astro/LangSwitcher.astro_astro_type_script_index_0_lang.DSY8g97O.js
@@ -0,0 +1,1 @@
+import{s as e}from"./i18n.DDnFYYmC.js";for(const t of document.querySelectorAll("[data-site-language]"))t.dataset.bound||(t.dataset.bound="1",t.addEventListener("click",()=>e(t.dataset.siteLanguage)));
